aebfebbf51481f9d9469c8bd25733e20a690d65c
pjmk007 <60164230+pjmk007@users.noreply.github.com>
2021-07-04 01:27:10 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+//int FibOld(int i)
+//{
+//    if (i <= 2)
+//    {
+//        return 1;
+//    }
+//    return FibOld(i - 2) + FibOld(i - 1);
+//}
+
+int Fib(int i) =>
+    i switch
+    {
+        int when i <= 2 => 1,
+        _ => Fib(i - 2) + Fib(i - 1)
+    };
+
+//int FibTail(int n)
+//{
+//    return Fib(n, 0, 1);
+//}
+
+//int Fib(int n, int left, int right)
+//{
+//    if (n == 0)
+//    {
+//        return 0;
+//    }
+//    else if (n == 1)
+//    {
+//        return right;
+//    }
+//    else
+//    {
+//        return Fib(n - 1, right, right + left);
+//    }
+//}
+
+Console.WriteLine($"43번째: {Fib(43)}");
+Console.WriteLine(Fib(43));
+Console.WriteLine($"3번째: {Fib(3)}");
